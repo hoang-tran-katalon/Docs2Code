db48cc410177846cfad526e3a3096621339fc22c
Hoang Tran <hoang.tran@katalon.com>
2021-06-02 17:50:38 +0700

    information extraction ver 1

--- extract_tutorials.py
+++ extract_tutorials.py
@@ -25,6 +25,10 @@ def get_sentences_from_url(url):
   sel = Selector(text=html)
 
   css_path = 'div#article-contents'
+  headline_css_path = 'h1.border-bottom-0::text'
+
+  headline = sel.css(headline_css_path).extract()[0]
+
   sentences = []
   for sentence in sel.css(css_path).extract():
     sentence = remove_tags(sentence)
@@ -32,7 +36,7 @@ def get_sentences_from_url(url):
     sentence = [string for string in sentence if string != ""]
     sentences.append(sentence)
 
-  return [item.strip() for sublist in sentences for item in sublist]
+  return headline, [item.strip() for sublist in sentences for item in sublist]
 
 
 def get_tutorials_from_url(url):
@@ -51,7 +55,7 @@ def get_tutorials_from_url(url):
 
 
 def predict_tutorials_from_url(url):
-  sentences = get_sentences_from_url(url)
+  headline, sentences = get_sentences_from_url(url)
   sequences_predict = tokenizer.texts_to_sequences(sentences)
   padded = pad_sequences(sequences_predict, maxlen=constants.MAX_LENGTH, padding=constants.PADDING_TYPE, truncating=constants.TRUNC_TYPE)
 
@@ -61,4 +65,6 @@ def predict_tutorials_from_url(url):
   result['sentences'] = sentences
   result['predicted'] = predicted
 
-  return result[result['predicted'] > 0.5]['sentences'].to_list()
+  print(result)
+
+  return headline, result[result['predicted'] > 0.3]['sentences'].to_list()
--- main.py
+++ main.py
@@ -6,17 +6,24 @@ import constants
 nlp = spacy.load("en_core_web_md")
 
 if __name__ == "__main__":
-    tutorials = extract_tutorials.predict_tutorials_from_url(constants.URL_TEST)
+    headline, tutorials = extract_tutorials.predict_tutorials_from_url(constants.URL_TEST)
     
-
-    sent = ' '.join(tutorials)
-    doc = nlp(sent)
-
     template = []
-    for chunk in list(doc.noun_chunks):
-        if chunk.root.dep_ in ['dobj', 'nsubj'] and chunk.root.head.dep_ in ['ROOT', 'conj', 'advcl']:
-            result = (chunk.root.head.text.capitalize(), chunk.text)
-            template.append(result)
-            
-            template = [item for item in template if item[0] not in ['Receive', 'Is', 'Happen']]
-    print(template)
+    template_string = '<Start> => '
+
+    for tutorial in tutorials:
+        tutorial = tutorial.replace('to ', '').replace(',', '')
+        doc = nlp(tutorial)
+        
+        for chunk in list(doc.noun_chunks):
+            if chunk.root.dep_ in ['dobj', 'nsubj'] and chunk.root.head.dep_ in ['ROOT', 'conj', 'advcl']:
+                result = (chunk.root.head.text.capitalize(), chunk.text)
+                template.append(result)
+                template = [item for item in template if item[0] not in ['Receive', 'Is', 'Happen']]
+                
+                template_string+=chunk.root.head.text.capitalize()+' '+chunk.text
+                template_string+=' => '
+
+    template_string+='<End>'
+    print(headline)
+    print(template_string)
